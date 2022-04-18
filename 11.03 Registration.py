3b7c7c5e6e2f2edf8d7f88090425ee0889c9d28e
parikshya098 <pbhandari1@ualr.edu>
2022-04-18 14:48:42 +0000

    Unit 11

--- 11.03 Registration.py
+++ 11.03 Registration.py
@@ -3,31 +3,31 @@ class Student():
         self.firstname = firstname
         self.lastname = lastname
         self.tnumber = tnumber
-        self.courseList = []
+        self.courses = []
 
 class StudentList():
     def __init__(self):
         self.studentList = []
-    def add_student(self, firstname, lastname, tnumber):
-        self.studentList.append(Student(firstname,lastname,tnumber))
-    def find_student(self, studenttofind):
-        for i in range(len(self.studentList)):
-            if(self.studentList[i].tnumber == studenttofind):
+        def add_student(self, firstname, lastname, tnumber):
+          self.studentList.append(Student(firstname,lastname,tnumber))
+        def find_student(self, studenttofind):
+          for i in range(len(self.studentList)):
+              if(self.studentList[i].tnumber == studenttofind):
                 return i;
                 return -1
-    def print_student_list(self):
-        print("Name\t\t\tTNumber\t\tCourses")
-        for student in self.studentList:
+        def print_student_list(self):
+          print("Name\t\t\tTNumber\t\tCourses")
+          for student in self.studentList:
             print(student.firstname,student.lastname,"\t\t",student.tnumber,"\t",end='')
-        for course in student.courses:
+          for course in student.courses:
             print(course.number,' ',end = ' ')
             print()
-    def add_student_from_file(self, filename):
-        file=open(filename)
-        lines=file.readlines()
-        for line in lines:
-            line=line.strip().split(',')
-            self.studentList.append(Student(line[0],line[1],line[2]))
+        def add_student_from_file(self, filename):
+            file=open(filename)
+            lines=file.readlines()
+            for line in lines:
+                line=line.strip().split(',')
+                self.studentList.append(Student(line[0],line[1],line[2]))
 
 class Course():
     def __init__(self,department, number, name, room, meetingtimes):
@@ -63,9 +63,9 @@ class CourseList():
             lines = file.readlines()
             for line in lines:
                line = line.strip().split(',')
-            index1 = studentList.find_student(line[0])
-            index2 = courseList.find_course(line[1],line[2])
-            if(index1>= 0 and index2>= 0):
-                studentList.studentList[index1].courses.append(Course(courseList.courseList[index2].department,courseList.courseList[index2].number,courseList.courseList[index2].name,courseList.courseList[index2].room,courseList.courseList[index2].meetingtimes))
+               index1 = studentList.find_student(line[0])
+               index2 = courseList.find_course(line[1],line[2])
+               if(index1>= 0 and index2>= 0):
+                   studentList.studentList[index1].courses.append(Course(courseList.courseList[index2].department,courseList.courseList[index2].number,courseList.courseList[index2].name,courseList.courseList[index2].room,courseList.courseList[index2].meetingtimes))
 
-            studentList.print_student_list()
+                   studentList.print_student_list()

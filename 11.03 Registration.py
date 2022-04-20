005a4b93d19250f9fab6b7666f9142fd2f9e29a0
parikshya098 <pbhandari1@ualr.edu>
2022-04-20 15:08:49 +0000

    Unit 11

--- 11.03 Registration.py
+++ 11.03 Registration.py
@@ -1,71 +1,109 @@
-class Student():
-    def __init__(self,firstname, lastname, tnumber):
-      self.firstname = firstname
-      self.lastname = lastname
-      self.tnumber = tnumber
-      self.courseList = []
+from prettytable import PrettyTable
 
-class StudentList():
-    def __init__(self):
-      self.studentList = []
-      def add_student(self, firstname, lastname, tnumber):
-        self.studentList.append(Student(firstname,lastname,tnumber))
-      def find_student(self, studenttofind):
-        for i in range(len(self.studentList)):
-            if(self.studentList[i].tnumber == studenttofind):
-              return i;
-              return -1
-      def print_student_list(self):
-        print("Name\t\t\tTNumber\t\tCourses")
-        for student in self.studentList:
-          print(student.firstname,student.lastname,"\t\t",student.tnumber,"\t",end='')
-        for course in student.courses:
-          print(course.number,' ',end = ' ')
-          print()
-      def add_student_from_file(self, filename):
-          filename = open('11.03 Students.txt')
-          lines = filename.readlines()
-          for line in lines:
-            line = line.strip().split(',')
-            self.studentList.append(Student(line[0],line[1],line[2]))
 
-class Course():
-    def __init__(self,department, number, name, room, meetingtimes):
-      self.department = department
-      self.number = number
-      self.name = name
-      self.room = room
-      self.meetingtimes = meetingtimes
+class Course:
+    def __init__(self, department="", number="", name="", room="", meetingtimes=""):
+        self.department = department
+        self.number = number
+        self.name = name
+        self.room = room
+        self.meetingtimes = meetingtimes
 
-class CourseList():
-    def __init__(self):
-      self.courseList=[]
-      def add_course(self, department, number, name, room, meetingtimes):
-          self.courseList.append(Course(department, number, name, room, meetingtimes))
-      def find_course(self, departmenttofind, numbertofind):
-        for i in range(len(self.courseList)):
-          if self.courseList[i].department==departmenttofind and self.courseList[i].number==numbertofind:
-            return i
-            return -1
-      def add_course_from_file(self, file):
-          file=open("11.03 Courses.txt")
-          lines=file.readlines()
-          for line in lines:
-            line=line.strip().split(',')
-            self.courseList.append(Course(line[0],line[1],line[2],line[3],line[4]))
 
-      if __name__ == "__main__":
-        courseList = CourseList()
-        courseList.CourseList("11.03 Courses.txt")
-        studentList = StudentList()
-        studentList.StudentList("11.03 Students.txt")
-        file = open("11.03 Registration.txt")
-        lines = file.readlines()
-        for line in lines:
-          line = line.strip().split(',')
-          index1 = studentList.find_student(line[0])
-          index2 = courseList.find_course(line[1],line[2])
-          if(index1>= 0 and index2>= 0):
-            studentList.studentList[index1].courses.append(Course(courseList.courseList[index2].department,courseList.courseList[index2].number,courseList.courseList[index2].name,courseList.courseList[index2].room,courseList.courseList[index2].meetingtimes))
+# Course List Class
+class CourseList:
+    def __init__(self, courseList=None):
+        if courseList is None:
+            courseList = []
+        self.courseList = courseList
 
-            studentList.print_student_list()
+    def add_course(self, department, number, name, room, meetingtimes):
+        temp = Course(department, number, name, room, meetingtimes)
+        self.courseList.append(temp)
+
+    def find_course(self, departmenttofind, numbertofind):
+        for c in range(len(self.courseList)):
+            if self.courseList[c].department == departmenttofind and self.courseList[c].number == numbertofind:
+                return c
+
+    def add_course_from_file(self, filename):
+        file = open(filename, "r")
+        for x in file:
+            courseInformation = x.split(",")
+            self.add_course(courseInformation[0], courseInformation[1], courseInformation[2], courseInformation[3],
+                            courseInformation[4].strip())
+
+    def print_course_list(self):
+        for tempCoursee in self.courseList:
+            print(tempCoursee)
+
+
+# Student Class
+class Student:
+    def __init__(self, firstname="", lastname="", tnumber=""):
+        self.enrolledCourses = []
+        self.firstname = firstname
+        self.lastname = lastname
+        self.tnumber = tnumber
+
+
+# Student List Class
+class StudentList:
+    def __init__(self, studentList=None):
+        if studentList is None:
+            studentList = []
+        self.studentList = studentList
+
+    def add_student(self, firstName, lastName, tnumber):
+        temp = Student(firstName, lastName, tnumber)
+        self.studentList.append(temp)
+
+    def find_student(self, studenttofind):
+        for studentIndex in range(len(self.studentList)):
+            if self.studentList[studentIndex].tnumber == studenttofind:
+                return studentIndex
+
+    def print_student_list(self):
+        t = PrettyTable(['First Name', 'Last Name', 'T-Number', 'Course', '', 'Name', ' Room', 'Meeting Times'])
+        t.border = False
+        t.align = "l"
+        for z in range(len(self.studentList)):
+            studentCourse = self.studentList[z].enrolledCourses
+            courses = "\n"
+            numbers = "\n"
+            names = "\n"
+            room = "\n"
+            meetingtimes = "\n"
+            for q in range(len(studentCourse)):
+                courses = courses + studentCourse[q].department
+                numbers = numbers + studentCourse[q].number
+                names = names + studentCourse[q].name
+                room = room + studentCourse[q].room
+                meetingtimes = meetingtimes + studentCourse[q].meetingtimes
+                if q != len(studentCourse) - 1:
+                    courses = courses + "\n"
+                    numbers = numbers + "\n"
+                    names = names + "\n"
+                    room = room + "\n"
+                    meetingtimes = meetingtimes + "\n"
+            t.add_row(
+                [self.studentList[z].firstname, self.studentList[z].lastname, self.studentList[z].tnumber, courses,
+                 numbers, names, room, meetingtimes])
+        print(t)
+
+    def add_student_from_file(self, filename):
+        file = open(filename, "r")
+        for f in file:
+            studentInformation = f.split(",")
+            self.add_student(studentInformation[0], studentInformation[1], studentInformation[2].strip())
+
+courseList1 = CourseList()
+courseList1.add_course_from_file("11.03 Courses.txt")
+studentList1 = StudentList()
+studentList1.add_student_from_file("11.03 Students.txt")
+regFile = open("11.03 Registration.txt", "r")
+for i in regFile:
+    line = i.split(",")
+    tempCourse = courseList1.courseList[courseList1.find_course(line[1], line[2].strip())]
+    studentList1.studentList[studentList1.find_student(str(line[0]))].enrolledCourses.append(tempCourse)
+studentList1.print_student_list()

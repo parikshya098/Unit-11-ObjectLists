d847d8a536bc1a5bbd9f871e875a5d00dc55be6e
parikshya098 <pbhandari1@ualr.edu>
2022-04-20 13:56:56 +0000

    Unit 11

--- 11.01 Retail Item.py
+++ 11.01 Retail Item.py
@@ -1,32 +1,55 @@
-class RetailItem: 
-    def __init__(self, Description='', UnitOnHand=0, Price=0): 
-        self.Description = Description 
-        self.UnitOnHand = UnitOnHand 
-        self.Price = Price 
-    def InventoryValue(self): 
-        return (self.UnitOnHand * self.Price) 
-inventoryfile = open("11.01 Inventory.txt","r")
-inventorylist = []
-x = inventoryfile.readline()
-while x !="":
-    description, unit_on_hand, price = x.split(",")
-    item = RetailItem(description, int(unit_on_hand), float(price))
-    inventorylist.append(item)
-    x = inventoryfile.readline()
-print("{:>20s}{:>20s}{:>20s}{:>20s}".format("Description","Units On Hand","Price","Inventory Value"))
-for i in range(len(inventorylist)):
-    print("{:>20s}{:>20d}{:>20.2f}{:>20.2f}".format(inventorylist[i].Description,inventorylist[i].UnitOnHand,inventorylist[i].Price,inventorylist[i].InventoryValue()))
+class Retail_Item:
+    def __init__(self, Description = "", UnitsOnHand = 0, Price = 0):   
+        self.Description = Description
+        self.UnitsOnHand = UnitsOnHand
+        self.Price = Price
+    
+    def Inventory_Value(self):       
+        return self.UnitsOnHand * self.Price
 
-updatedfile = open("11.01 InventoryUpdate.txt", "r")
-updatedlist = []
-y = updatedfile.readline()
-while y !="":
-    description, price = y.split(",")
-    item = RetailItem(description, int(unit_on_hand), float(price))
-    updatedlist.append(item)
-    y = updatedfile.readline()
-print("{:>20s}{:>20s}{:>20s}{:>20s}".format("Description","Units On Hand","Price","Inventory Value"))
-for i in range(len(updatedlist)):
-    print("{:>20s}{:>20d}{:>20.2f}{:>20.2f}".format(inventorylist[i].Description,inventorylist[i].UnitOnHand,updatedlist[i].Price,updatedlist[i].InventoryValue()))
+def to_print_inventory(inventory_list):       
+    print("Description\tUnits On Hand\tPrice\tInventory Value")    
+    for elem in inventory_list:                                   
+        obj = Retail_Item(elem[0], int(elem[1]), float(elem[2]))      
+        print("{:>11}\t{:>13}\t{:>5}\t{:>15.2f}".format(elem[0], elem[1], elem[2], obj.Inventory_Value()))
 
-print()
+def to_find_inventory(inventory_list, Description):   
+    for i in range(len(inventory_list)):
+        if (inventory_list[i][0] == Description):
+            return i
+    return -1
+
+def main():                         
+    inventory_list = []             
+    filepath = "11.01 Inventory.txt"
+    
+    with open(filepath) as fp:
+        while True:
+            text = fp.readline().strip()
+            if not text:
+                break
+            
+            inventory_list.append(text.split(", "))
+
+    to_print_inventory(inventory_list) 
+    print("\n")
+
+    inventory_update_list = []         
+    filepath1 = "11.01 InventoryUpdate.txt"   
+    with open(filepath1) as fp1:
+        while True:
+            text = fp1.readline().strip()
+            if not text:
+                break
+    
+            inventory_update_list.append(text.split(", "))
+
+    for elem in inventory_update_list:
+        pos = to_find_inventory(inventory_list, elem[0])
+        if (pos != -1):
+            inventory_list[pos][2] = elem[1]
+    
+    to_print_inventory(inventory_list)      
+        
+if __name__ == "__main__":
+    main()      

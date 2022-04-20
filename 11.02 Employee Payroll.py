ff63aaae8a91d644e0321b53af96412bca9072a2
parikshya098 <pbhandari1@ualr.edu>
2022-04-20 14:19:41 +0000

    Unit 11

--- 11.02 Employee Payroll.py
+++ 11.02 Employee Payroll.py
@@ -1,31 +1,46 @@
+from typing import List
+
 class Employee:
-    def __init__(self,firstname,lastname,id,hours,wage):
-        self.FirstName = firstname
-        self.LastName = lastname
-        self.IDNumber = id
-        self.HoursWorked = 0
-        self.Wage = wage
-    def WeeklyPay(self):
-        if self.HoursWorked <= 40:
-            return self.HoursWorked*self.Wage
-        return (40*self.Wage)+((self.HoursWorked-40)*1.5*self.Wage)
+   def __init__(self, firstname, lastname, idnumber, wage, hoursworked = 0):
+       self.firstname = firstname
+       self.lastname = lastname
+       self.idnumber = int(idnumber)
+       self.hoursworked = float(hoursworked)
+       self.wage = float(wage)
+   def WeeklyPay(self):
+       if self.hoursworked > 40:
+           return self.wage * 40 + self.wage * 1.5 * (self.hoursworked - 40)
+       return self.hoursworked * self.wage
 
-print("   First    Last      ID   Hours  Hourly  Weekly\n    Name    Name  Number  Worked    Wage     Pay""")
+def find_employee(employees : List[Employee], empid):
+   for i in range(len(employees)):
+       if empid == employees[i].idnumber:
+           return i
+   return -1
 
-file = open("11.02 Employees.txt", "r")
-
-for line in file.readlines():
-    e = line.split(',')
-    e = Employee(e[0].strip(),e[1].strip(),int(e[2]),float(e[3]), float(e[4]))
-
-file1 = open("11.02 Hours.txt", "r")
-
-for line1 in file1.readlines():
-        f = line1.split(',')
-        f = Employee(int(f[0]), float(f[1])) 
-
-    # print employee details
-print("%8s%8s%8d%8.2f%8.2f%8.2f"%(e.FirstName,e.LastName,e.IDNumber,f.HoursWorked,e.Wage,e.WeeklyPay()))
-
-# close file
-file.close()
+if __name__ == '__main__':
+   emp_file = '11.02 Employees.txt'
+   hours_file = '11.02 Hours.txt'
+   fh = open(emp_file)
+   employees = []
+   for line in fh:
+       line = line.strip('\n').split(',')
+       firstname, lastname, idnumber, wage = line[0], line[1], \
+                                           int(line[2]), float(line[3])
+       employee = Employee(firstname, lastname, idnumber, wage)
+       employees.append(employee)
+   fh = open(hours_file)
+   for line in fh:
+       line = line.strip('\n').split(',')
+       idnumber, hours = int(line[0]), float(line[1])
+       index = find_employee(employees, idnumber)
+       if index != -1:
+           employees[index].hoursworked = hours
+   print('{0:13s}{1:13s}{2:10s}  {3:14s}  {4:14s}{5:14s}'.format(
+       'First Name', 'Last Name', 'ID Number', 'Hours Worked',
+       'Hourly Wage', 'Weekly Pay'
+   ))
+   for emp in employees:
+       print('{0:13s}{1:13s}{2:10d}{3:14.2f}{4:14.2f}{5:14.2f}'.format(
+          emp.firstname, emp.lastname, emp.idnumber,
+           emp.hoursworked, emp.wage, emp.WeeklyPay()))
